e8a587ec83518eed5304737c04dfac6fd7eaef64
Pier-Luc Richard <pier-lucrichard@Pier-Lucs-MacBook-Air.local>
2022-05-30 20:01:18 -0400

    first day may 30 2022

---
+++ c00/ex00/megaphone.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+
+int main(int argc, char **argv)
+{
+
+	if (argc == 2)
+	{
+		std::cout << argv[1] << '\n';
+	}
+	else
+		std::cout <<" * LOUD AND UNBEARABLE FEEDBACK NOISE * "<< '\n';
+
+}
