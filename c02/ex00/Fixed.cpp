72fe4054c66e3963f2d83c96c6d12d7a5c8c333e
Pier-Luc Richard <pier-lucrichard@Pier-Lucs-MacBook-Air.local>
2022-06-27 15:16:45 -0400

    ex02 from c02 done

--- c02/ex00/Fixed.cpp
+++ c02/ex00/Fixed.cpp
@@ -1,14 +1,43 @@
 #include "Fixed.hpp"
 
-Fixed::Fixed(void){
+const int Fixed::bits = 8;
+
+//Constructeur standard ou je défini que value est 0
+Fixed::Fixed() : value(0){
 	std::cout << ("Default constructor callded") << std::endl;
 }
 
+//constructeur de recopie
+//create a new object
+Fixed::Fixed(const Fixed& old_obj){
+	std::cout << ("Copy constructor called") << std::endl;
+	*this = old_obj;
+}
 
+//opérateur d'affectation(copy assignment operator)
+//Assigns a value to an existing object
+//it is always called on an object that has already been constructed
+Fixed& Fixed::operator=(const Fixed& obj){
+
+	std::cout << "Copy assignment operator called" << std::endl;
+	this->value = obj.getRawBits();
+
+	return *this;
+}
+
+//Destructeur standard
 Fixed::~Fixed(void){
-	std::cout << ("Default constructor callded") << std::endl;
+	std::cout << ("Destructor called") << std::endl;
 }
 
-Fixed::Fixed(Const Fixed&){
-	
+
+//GetBits
+int Fixed::getRawBits(void) const{
+	std::cout << ("getRawBits member function called") << std::endl;
+	return (this->value);
+}
+//SetBits
+void	Fixed::setRawBits (int const raw){
+
+	this->value = raw;
 }
